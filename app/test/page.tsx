08415b9b0e5d765e482cf1fe364bdfd18f73fdb1
chiyoi <chiyoi2140@outlook.com>
2024-02-12 23:41:42 +0800

    tidy

--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-export default () => (
-  <iframe src='/game' allow={
-    'autoplay; fullscreen *; geolocation; microphone; camera; midi; monetization; xr-spatial-tracking; gamepad; gyroscope; accelerometer; xr'
-  } style={{
-    width: '100vw',
-    height: '100vh',
-    border: 'none',
-  }} />
-)
