1299cd506c19ded307af3435e0c4d845d13869c3
chiyoi <chiyoi2140@outlook.com>
2024-04-16 19:44:30 +0800

    tidy

--- functions/icon.png.ts
+++ functions/icon.png.ts
@@ -3,7 +3,7 @@ import { Env } from './common/env'
 
 export const onRequest: PagesFunction<Env> = async ({ env }) => {
   const { assets } = env
-  const item = await assets.get('Icons/op1.png')
+  const item = await assets.get('Icons/neko03.png')
   if (item === null) return error(404)
   const headers = new Headers()
   item.writeHttpMetadata(headers)
