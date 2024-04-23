3858655d80286a2b2df563b9d3ac3d18fbe6f6c6
chiyoi <chiyoi2140@outlook.com>
2024-04-23 14:23:05 +0800

    Tidy: Internal game iframe

---
+++ app/game-of-life/page.tsx
@@ -0,0 +1,9 @@
+import { Box, Card, Container, Flex, ScrollArea } from '@radix-ui/themes'
+
+export default () => (
+  <Container pt='9' px='5'>
+    <Card mx='auto' style={{ width: 'min-content' }}>
+      <iframe src='https://black-pond-052946110.5.azurestaticapps.net' width='640' height='360' />
+    </Card>
+  </Container>
+)
